47dca2a90ecab1df9422b11ada48640a0dd213e8
Dichik <omeluan.dima@gmail.com>
2023-01-21 10:01:08 +0200

    Add basic movement for robot

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import pygame
 import sys
+
+from constants import BLOCK_SIZE
+from robot import Robot
+
 pygame.init()
 
 display = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 
+display_scroll = [0, 0]
 
-class Player:
-
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
-    def main(self, display):
-        pygame.draw.rect(display, (255, 0, 0), (self.x, self.y, self.width, self.height))
-
-
-player = Player(400, 300, 32, 32)
+player = Robot(400, 300, BLOCK_SIZE, BLOCK_SIZE)
 
 while True:
     display.fill((24, 164, 86))
@@ -29,5 +22,18 @@ while True:
 
     player.main(display)
 
-    clock.tick(60)
+    keys = pygame.key.get_pressed()
+
+    pygame.draw.rect(display, (255, 255, 255), (100 - display_scroll[0], 100 - display_scroll[1], BLOCK_SIZE, BLOCK_SIZE))
+
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        display_scroll[0] -= BLOCK_SIZE
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        display_scroll[0] += BLOCK_SIZE
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        display_scroll[1] -= BLOCK_SIZE
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        display_scroll[1] += BLOCK_SIZE
+
+    clock.tick(20)
     pygame.display.update()
