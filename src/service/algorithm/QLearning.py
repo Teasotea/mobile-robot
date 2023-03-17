2061f734de6533cfb7164aca8ac535b8f9b10d27
Dichik <omeluan.dima@gmail.com>
2023-03-17 13:59:39 +0200

    update qlearning logic. can see walls

--- src/service/algorithm/QLearning.py
+++ src/service/algorithm/QLearning.py
@@ -21,8 +21,8 @@ class QLearning:
         self.collector = Collector()
 
         # Hyperparams
-        self.n_episodes = 5000  # 10000
-        self.max_iter_episode = 100
+        self.n_episodes = 500  # 10000
+        self.max_iter_episode = 5000
         self.exploration_proba = 0.9
         self.exploration_decreasing_decay = 0.001
         self.min_exploration_proba = 0.01
@@ -43,25 +43,24 @@ class QLearning:
     #     return 1 if self.robot.battery.current <= 200 else 0
 
     def restoreCans(self):
-        self.temp_cans = copy.deepcopy(self.cans)
+        self.temp_cans = self.cans[:]
 
-    def step(self, action):
+    def step(self, action, cans):
         reward = 0
 
         # TODO: implement check whether there is a wall
         # FIXME: rewrite this
-        if 60 > self.current_pos[0] + self.possibleDirections[action][0] >= 0 and 60 > self.current_pos[1] + \
-                self.possibleDirections[action][1] >= 0:
+        if 60 > self.current_pos[0] + self.possibleDirections[action][0] >= 0 \
+                and 60 > self.current_pos[1] + self.possibleDirections[action][1] >= 0:
             self.current_pos[0] += self.possibleDirections[action][0]
             self.current_pos[1] += self.possibleDirections[action][1]
         else:
             reward -= 100
 
-        # state = self.getState()
         next_state = self.current_pos[0] * self.current_pos[1]  # fix this
 
-        if self.robot.battery.current == 0:
-            reward -= 500
+        # if self.robot.battery.current == 0:
+        #     reward -= 500
 
         if not self.collector.isVisited(self.current_pos):
             reward += 20
@@ -71,11 +70,13 @@ class QLearning:
             reward -= 25
 
         # FIXME if current position in unused cans
-        # QUESTION: is there a possibility to be always at position with can?
-        if tuple(self.current_pos) in self.temp_cans:  # if can +200
-            print("REWARD", len(self.temp_cans))
+        current = (self.current_pos[0], self.current_pos[1])
+        if current in cans:
+            print("REWARD", len(cans))
             reward += 200
-            self.temp_cans.remove(tuple(self.current_pos))
+            cans.remove(current)
+        elif self.grid.isWall(current[0], current[1]):
+            reward -= 100
 
         # if state == 1 and self.currentPos == self.charger.getChargerPosition():
         #     reward += 10
@@ -90,7 +91,6 @@ class QLearning:
         return next_state, reward, done
 
     def build_QTable(self):
-        # Initialize the Q-table to 0
         n_observations = 60 * 60  # - 283, 60x60 board - bricks and 2 states
         n_actions = len(self.possibleDirections)
         return np.zeros((n_observations, n_actions))
@@ -102,57 +102,45 @@ class QLearning:
         for e in range(self.n_episodes):
             # we initialize the first state of the episode
             # start = self.robotCoordinates()
-            start = self.current_pos
             current_state = 0  # env.reset()
-            done = False
-            self.restoreCans()
-
-            # sum the rewards that the agent gets from the environment
+            cans = self.temp_cans[:]
             total_episode_reward = 0
 
             for i in range(self.max_iter_episode):
-                # we sample a float from a uniform distribution over 0 and 1
-                # if the sampled flaot is less than the exploration proba
-                #     the agent selects arandom action
-                # else
-                #     he exploits his knowledge using the bellman equation
 
                 if np.random.uniform(0, 1) < self.exploration_proba:
                     action = random.choice(
-                        range(len(self.possibleDirections)))  # env.action_space.sample() #choose random action
+                        range(len(self.possibleDirections))  # Choose random action
+                    )
                 else:
-                    action = np.argmax(QTable[current_state, :])  # choose the best action
+                    action = np.argmax(QTable[current_state, :])  # Choose the best action in QTable
 
                 # print(current_state, action, QTable[current_state, action])
 
-                next_state, reward, done = self.step(action)
+                next_state, reward, done = self.step(action, cans)
 
                 # print(self.current_pos)
                 # print(next_state, reward, done)
 
                 # We update our Q-table using the Q-learning iteration
-                QTable[current_state, action] = (1 - self.lr) * QTable[current_state, action] + self.lr * (
-                            reward + self.gamma * max(QTable[next_state, :]))
-                total_episode_reward = total_episode_reward + reward
+                QTable[current_state, action] = (1 - self.lr) * QTable[current_state, action] \
+                                                + self.lr * (reward + self.gamma * max(QTable[next_state, :]))
+                total_episode_reward += reward
+                current_state = next_state
                 # If the episode is finished, we leave the for loop
                 if done:
                     break
-                current_state = next_state
 
             # We update the exploration proba using exponential decay formula
-            exploration_proba = max(self.min_exploration_proba, np.exp(-self.exploration_decreasing_decay * e))
+            self.exploration_proba = max(self.min_exploration_proba, np.exp(-self.exploration_decreasing_decay * e))
             total_rewards_episode.append(total_episode_reward)
-            print("Training finished.\n")
-            # print(QTable)
-            # print(total_rewards_episode)
-            df = pd.DataFrame(QTable)
-            df.to_csv("data/qtable.csv")
 
+        self.saveState(QTable)
         return total_rewards_episode, QTable
 
     def getCollector(self):
         return self.collector
 
-    # def solve(self):
-
-    #     return path
+    def saveState(self, qtable):
+        df = pd.DataFrame(qtable)
+        df.to_csv("data/qtable.csv")
